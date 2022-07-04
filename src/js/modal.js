d36b1297dcad415c6b40457de17f7488fb48a3ee
krishna-saini <krishnapraanjal10@gmail.com>
2022-07-04 16:40:36 +0530

    clone commits

---
+++ src/js/modal.js
@@ -0,0 +1,26 @@
+export const state = {
+  recipe: {},
+  search: {},
+};
+
+export const loadRecipe = async function (id) {
+  //1) laoding recipe
+  const res = await fetch(
+    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+  );
+  const data = await res.json();
+  if (!res.ok) throw new Error(`${data.message}`);
+
+  const { recipe } = data.data;
+
+  state.recipe = {
+    title: recipe.title,
+    publisher: recipe.publisher,
+    sourceUrl: recipe.source_url,
+    servings: recipe.servings,
+    cookingTime: recipe.cooking_time,
+    ingredients: recipe.ingredients,
+    image: recipe.image_url,
+  };
+  console.log(state.recipe);
+};
