9ac20cdcc33b655f04a216b70ac86bee6b96d29e
krishna-saini <71994309+krishna-saini@users.noreply.github.com>
2022-07-13 16:21:21 +0530

    new api key added

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
 export const TIMEOUT_SEC = 10;
 export const RESULTS_PER_PAGE = 10;
-export const KEY = "7177ac6b-96e2-4d33-a91c-06b964b5f7c9";
+export const KEY = "ff99f9e7-6dd8-4cae-a7c0-5471268730a3";
 export const MODAL_CLOSE_SEC = 5;
